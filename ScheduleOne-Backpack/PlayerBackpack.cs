6f2d22baa256fd1bf9b4ab3d08a7f7a0418d8b8f
Damian Kraaijeveld <damian.kraaijeveld96@gmail.com>
2025-04-10 01:08:56 +0200

    feat: initial work for proper storage handling

---
+++ ScheduleOne-Backpack/PlayerBackpack.cs
@@ -0,0 +1,153 @@
+﻿using Il2CppScheduleOne.ItemFramework;
+using Il2CppScheduleOne.Persistence.Datas;
+using Il2CppScheduleOne.PlayerScripts;
+using MelonLoader;
+using UnityEngine;
+
+namespace BackpackMod;
+
+[RegisterTypeInIl2Cpp]
+public class PlayerBackpack : MonoBehaviour
+{
+    private const KeyCode ToggleKey = KeyCode.B;
+
+    private bool _backpackEnabled = true;
+    private BackpackStorage _backpackStorage;
+
+    public PlayerBackpack(IntPtr ptr) : base(ptr)
+    {
+    }
+
+    public static bool InstanceExists => Instance != null;
+
+    public static PlayerBackpack Instance { get; protected set; }
+
+    public bool IsOpen => _backpackStorage.IsOpen;
+
+    public void Awake()
+    {
+        _backpackStorage = gameObject.AddComponent<BackpackStorage>();
+        OnStartClient(true);
+    }
+
+    public void Update()
+    {
+        if (!Input.GetKeyDown(ToggleKey))
+            return;
+
+        try
+        {
+            if (IsOpen)
+                Close();
+            else
+                Open();
+        }
+        catch (Exception e)
+        {
+            Melon<BackpackMod>.Logger.Error("Error toggling backpack: " + e.Message);
+        }
+    }
+
+    public void SetBackpackEnabled(bool enabled)
+    {
+        _backpackEnabled = enabled;
+        if (!enabled && IsOpen)
+            _backpackStorage.Close();
+    }
+
+    public void Open()
+    {
+        if (!_backpackEnabled || IsOpen)
+        {
+            Melon<BackpackMod>.Logger.Warning("Backpack is already open or backpack is disabled.");
+            return;
+        }
+
+        if (_backpackStorage == null)
+        {
+            Melon<BackpackMod>.Logger.Error("BackpackStorage is null!");
+            return;
+        }
+
+        _backpackStorage.Open();
+    }
+
+    public void Close()
+    {
+        if (!_backpackEnabled || !IsOpen)
+            return;
+
+        _backpackStorage.Close();
+    }
+
+    public string GetBackpackString()
+    {
+        if (_backpackStorage == null)
+        {
+            Melon<BackpackMod>.Logger.Error("BackpackStorage is null!");
+            return string.Empty;
+        }
+
+        return new ItemSet(_backpackStorage.ItemSlots).GetJSON();
+    }
+
+    public void LoadBackpack(string contentsString)
+    {
+        if (string.IsNullOrEmpty(contentsString))
+        {
+            Melon<BackpackMod>.Logger.Warning("Empty backpack string");
+            return;
+        }
+
+        if (!Player.Local.IsOwner || _backpackStorage == null)
+            return;
+
+        var items = ItemSet.Deserialize(contentsString);
+        if (items == null)
+        {
+            Melon<BackpackMod>.Logger.Error("Failed to deserialize backpack string");
+            return;
+        }
+
+        for (var i = 0; i < items.Count; i++)
+        {
+            var item = items[i];
+            if (item == null)
+                continue;
+
+            if (i >= _backpackStorage.SlotCount)
+            {
+                Melon<BackpackMod>.Logger.Error($"Item slot index {i} out of range");
+                break;
+            }
+
+            _backpackStorage.ItemSlots[new Index(i)].Cast<ItemSlot>().SetStoredItem(item);
+        }
+    }
+
+    public void OnStartClient(bool isOwner)
+    {
+        if (!isOwner)
+        {
+            Melon<BackpackMod>.Logger.Msg("Destroying non-local player singleton: " + name, null);
+            Destroy(this);
+            return;
+        }
+
+        if (Instance != null)
+        {
+            Melon<BackpackMod>.Logger.Warning("Multiple instances of " + name + " exist. Keeping prior instance reference.", null);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null!;
+        }
+    }
+}
---
+++ ScheduleOne-Backpack/PlayerPatch.cs
@@ -0,0 +1,91 @@
+﻿using HarmonyLib;
+using Il2CppScheduleOne.Persistence;
+using Il2CppScheduleOne.Persistence.Datas;
+using Il2CppScheduleOne.PlayerScripts;
+using MelonLoader;
+
+namespace BackpackMod;
+
+[HarmonyPatch(typeof(Player))]
+public static class PlayerPatch
+{
+    [HarmonyPatch("Awake")]
+    [HarmonyPrefix]
+    public static void Awake(Player __instance)
+    {
+        if (__instance.LocalExtraFiles.Contains("Backpack"))
+        {
+            Melon<BackpackMod>.Logger.Msg("Player already has backpack data.");
+            return;
+        }
+
+        Melon<BackpackMod>.Logger.Msg("Adding backpack data to player.");
+        __instance.LocalExtraFiles.Add("Backpack");
+    }
+
+    [HarmonyPatch("WriteData")]
+    [HarmonyPostfix]
+    [HarmonyWrapSafe]
+    public static void WriteData(Player __instance, string parentFolderPath)
+    {
+        var backpackStorage = PlayerBackpack.Instance.GetBackpackString();
+        __instance.Cast<ISaveable>().WriteSubfile(parentFolderPath, "Backpack", backpackStorage);
+    }
+
+    [HarmonyPatch("Load", typeof(PlayerData), typeof(string))]
+    [HarmonyPostfix]
+    [HarmonyWrapSafe]
+    public static void Load(Player __instance, PlayerData data, string containerPath)
+    {
+        if (!__instance.Loader.TryLoadFile(containerPath, "Backpack", out var contentsString))
+            return;
+
+        try
+        {
+            PlayerBackpack.Instance.LoadBackpack(contentsString);
+        }
+        catch (Exception e)
+        {
+            Melon<BackpackMod>.Logger.Error($"Error loading backpack data: {e.Message}");
+        }
+    }
+
+    [HarmonyPatch("Activate")]
+    [HarmonyPrefix]
+    [HarmonyWrapSafe]
+    public static void Activate()
+    {
+        Melon<BackpackMod>.Logger.Msg("Activating backpack");
+        PlayerBackpack.Instance.SetBackpackEnabled(true);
+    }
+
+    [HarmonyPatch("Deactivate")]
+    [HarmonyPrefix]
+    [HarmonyWrapSafe]
+    public static void Deactivate()
+    {
+        Melon<BackpackMod>.Logger.Msg("Deactivating backpack");
+        PlayerBackpack.Instance.SetBackpackEnabled(false);
+    }
+
+    [HarmonyPatch("ExitAll")]
+    [HarmonyPrefix]
+    [HarmonyWrapSafe]
+    public static void ExitAll()
+    {
+        Melon<BackpackMod>.Logger.Msg("Exiting all backpacks");
+        PlayerBackpack.Instance.SetBackpackEnabled(false);
+    }
+
+    [HarmonyPatch("OnDied")]
+    [HarmonyPrefix]
+    [HarmonyWrapSafe]
+    public static void OnDied(Player __instance)
+    {
+        if (!__instance.Owner.IsLocalClient)
+            return;
+
+        Melon<BackpackMod>.Logger.Msg("Player died, disabling backpack");
+        PlayerBackpack.Instance.SetBackpackEnabled(false);
+    }
+}
